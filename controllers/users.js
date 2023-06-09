5366ca3a57bc19d1971980abebf186b482b8edf7
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-09 15:41:33 -0400

    add route for GET all users in DB

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,12 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+// Fetch all users in DB
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 // Create a new user
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
