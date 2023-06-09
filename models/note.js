078df8448311f0d6c12a42e17789d2cf643c74e0
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-09 13:53:44 -0400

    expand to contain info of user who created note

--- models/note.js
+++ models/note.js
@@ -10,6 +10,10 @@ const noteSchema = new mongoose.Schema({
     type: Boolean,
     required: true
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 // Transform note schema formatting
