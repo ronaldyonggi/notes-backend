5de3f9d667f7e4b0d1da1366d1a5c49c66c18705
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-09 15:16:18 -0400

    created tests for users api

---
+++ tests/user_api.test.js
@@ -0,0 +1,68 @@
+const bcrypt = require('bcrypt')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+const { default: mongoose } = require('mongoose')
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const rootUser = new User({username: 'root', passwordHash})
+
+    await rootUser.save()
+  })
+
+  test('creation succeeds with username that has not been used', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+
+    // Check if the number of users in DB increased
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation failed with proper status code and message if username is already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    // Check if the number of users in DB stays the same
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
