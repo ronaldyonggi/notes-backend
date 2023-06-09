81e987898688138c3a820d0f24ffc860f3cac42e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-09 14:45:45 -0400

    routing for users

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+// Create a new user
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const newUser = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await newUser.save()
+  response.status(201).json(savedUser)
+})
